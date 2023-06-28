114084fa64f79250b0b0b1ae4bfae47405b2b02a
Leonel Moran <leonelzecena2020@gmail.com>
2023-06-28 16:57:32 -0600

    Insertar la tarjeta seleccionado al DOM

--- Carrito-compras/js/app.js
+++ Carrito-compras/js/app.js
@@ -18,3 +18,43 @@ function cargarEventListeners() {
     vaciarCarritoBtn.addEventListener('click', vaciarCarrito);
 
 }
+
+// Función que añade el curso al carrito
+function agregarCurso(e) {
+    e.preventDefault();
+    // Delegation para agregar-carrito
+    if(e.target.classList.contains('agregar-carrito')) {
+         const curso = e.target.parentElement.parentElement;
+         // Enviamos el curso seleccionado para tomar sus datos
+         leerDatosCurso(curso);
+    }
+}
+
+function leerDatosCurso(curso) {
+    const infoCurso = {
+         imagen: curso.querySelector('img').src,
+         titulo: curso.querySelector('h4').textContent,
+         precio: curso.querySelector('.precio span').textContent,
+         id: curso.querySelector('a').getAttribute('data-id'), 
+         cantidad: 1
+    }
+
+    //Agrega elementos al  arreglo de carrito
+    if( articulosCarrito.some( curso => curso.id === infoCurso.id ) ) { 
+         const cursos = articulosCarrito.map( curso => {
+              if( curso.id === infoCurso.id ) {
+                   curso.cantidad++;
+                    return curso;
+               } else {
+                    return curso;
+            }
+         })
+         articulosCarrito = [...cursos];
+    }  else {
+         articulosCarrito = [...articulosCarrito, infoCurso];
+    }
+
+    console.log(articulosCarrito)
+
+    carritoHTML();
+}
