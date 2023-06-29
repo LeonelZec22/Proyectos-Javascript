a332fdecb34bc6ae4d38ce7a8ca065fff1ab26da
Leonel Moran <leonelzecena2020@gmail.com>
2023-06-28 21:54:18 -0600

    editando

---
+++ Enviar-email/js/app.js
@@ -0,0 +1,12 @@
+const correo = document.querySelector('#correo');
+const email = document.querySelector('#email');
+const asunto = document.querySelector('#asunto');
+const mensaje = document.querySelector('#mensaje');
+
+const btnEnviar = document.querySelector('#enviar');
+const formularioEnviar = document.querySelector('#enviar-mail');
+const resetBtn = document.querySelector('#resetBtn');
+
+// event Listener
+
+eventListeners();
